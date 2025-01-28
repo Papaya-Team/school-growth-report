2e9e3efc5fe9679754956c9b63f4a8aed0eda337
DanielPapay <DanielPapay@users.noreply.github.com>
2025-01-28 17:27:38 +0000

    deploy: b1af97f9941dcd429ce54d2a68672197e1a27560

--- assets/StudentProfile-BdBJWUGa.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./BackBtn.vue_vue_type_script_setup_true_lang-DFKJvzjP.js";import{d as n,c as s,e as a,o as r}from"./index-BT354If1.js";const c={class:"main-container"},l=n({__name:"StudentProfile",setup(i){return(o,e)=>(r(),s("section",c,[a(t,{onClick:e[0]||(e[0]=m=>o.$router.back()),class:"my-[20px]"})]))}});export{l as default};
